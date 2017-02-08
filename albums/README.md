1b95fcf143abc3a36275efd94e051e40108969e8
ridwanfansuri <ridwan@icehousecorp.com>
2017-02-08 11:09:57 +0700

    initial

---
+++ albums/README.md
@@ -0,0 +1,4 @@
+# react-native-albums
+repository for complete guide react native udemy
+
+https://www.udemy.com/the-complete-react-native-and-redux-course/
---
+++ albums/index.android.js
@@ -0,0 +1,17 @@
+//index.ios.js - place code here for ios
+
+//import libraryto help create a Component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+//create a Component
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+  );
+
+//render it to the device
+AppRegistry.registerComponent('albums', () => App);
